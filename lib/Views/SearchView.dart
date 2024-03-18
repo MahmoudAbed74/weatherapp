e66ad38e2c1ba3cb567c7b18da70d8d906dc4ca8
Mahmoud Abed <mahmoudabed120@gmail.com>
2024-03-19 01:13:40 +0200

    Update SearchView layout and styling
    Refactor SearchView layout to improve readability and user experience.
    Adjust padding, icons, text styles, and input field decoration for better
    aesthetic appeal and functionality.

--- lib/Views/SearchView.dart
+++ lib/Views/SearchView.dart
@@ -7,13 +7,24 @@ class SearchView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Search View"),
+        title: const Text("Search View"),
       ),
-      body: Center(
-        child: TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: "Enter City Name",
+              suffixIcon: const Icon(Icons.search),
+              prefixIcon: const Icon(Icons.location_on),
+              labelText: "Search",
+              labelStyle: const TextStyle(
+                  color: Colors.black, fontWeight: FontWeight.bold),
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
             ),
           ),
         ),
